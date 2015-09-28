af2be41fb7de3f99b68a5fe0cf4ea8433c53675f
Güicho Suárez <luissuarezdev@gmail.com>
2015-09-28 13:19:31 -0430

    Se agregan funciones extras a las notificaciones

--- src/js/ibsen.buzz.js
+++ src/js/ibsen.buzz.js
@@ -26,10 +26,11 @@
             if ($Ibsen.buzz.desktop.support.hasSupport()) {
                 var $Notification = $Ibsen.buzz.desktop.support.Notification();
 
-                $options = $.extend({}, $Ibsen.buzz.default.desktop, object || {});
+                $options = $.extend({}, $Ibsen.buzz.default.desktop, ($Ibsen.isValidType(object, 'object') ? object : {}));
 
                 if ($Notification.permission === "granted") {
-                    return new $Notification($options.title, $options);
+                    $Ibsen.buzz.desktop.support._this = new $Notification($options.title, $options)
+                    return $Ibsen.buzz.desktop;
                 } else if ($Notification.permission !== 'denied') {
                     $Notification.requestPermission(function (permission) {
                         if (permission === "granted") {
@@ -42,7 +43,35 @@
             }
         };
 
+        $Ibsen.buzz.desktop.on = {
+            show : function (fn) {
+                if ($Ibsen.isValidType(fn, 'function')) {
+                    $Ibsen.buzz.desktop.support._this.onshow = fn();
+                }
+                return $Ibsen.buzz.desktop.on;
+            },
+            click: function (fn) {
+                if ($Ibsen.isValidType(fn, 'function')) {
+                    $Ibsen.buzz.desktop.support._this.onclick = fn;
+                }
+                return $Ibsen.buzz.desktop.on;
+            },
+            close: function (fn) {
+                if ($Ibsen.isValidType(fn, 'function')) {
+                    $Ibsen.buzz.desktop.support._this.onclose = fn;
+                }
+                return $Ibsen.buzz.desktop.on;
+            },
+            error: function (fn) {
+                if ($Ibsen.isValidType(fn, 'function')) {
+                    $Ibsen.buzz.desktop.support._this.onerror = fn;
+                }
+                return $Ibsen.buzz.desktop.on;
+            }
+        };
+
         $Ibsen.buzz.desktop.support = {
+            _this       : null,
             hasSupport  : function () {
                 if (($Ibsen.hasSupport('Notification') || $Ibsen.hasSupport('mozNotification') || $Ibsen.hasSupport('webkitNotification'))) {
                     return true;
@@ -53,8 +82,7 @@
             },
             Notification: function () {
                 return window.Notification || window.mozNotification || window.webkitNotification;
-            },
-            default     : {}
+            }
         };
     }
 })(window.jQuery, window.$Ibsen);
